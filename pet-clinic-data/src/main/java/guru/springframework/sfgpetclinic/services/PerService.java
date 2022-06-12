c816a801478d0bfa3c786eb8344d84741ff1474f
meetsridhar9 <meetsridhar9@gmail.com>
2022-06-12 00:04:39 -0700

    Initial Project Add

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PerService.java
@@ -2,12 +2,10 @@ package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.model.Pet;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.Set;
 
-public interface PerService {
+public interface PerService extends CrudRepository<Pet, Long> {
 
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
 }
