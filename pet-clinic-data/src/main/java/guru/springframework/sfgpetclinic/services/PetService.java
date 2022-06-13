3c3f70d121440e649ecb71fff8475956ae0fb142
meetsridhar9 <meetsridhar9@gmail.com>
2022-06-12 17:46:53 -0700

    Map Services changes

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
@@ -1,11 +1,8 @@
 package guru.springframework.sfgpetclinic.services;
 
-import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.model.Pet;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Set;
-
-public interface PerService extends CrudRepository<Pet, Long> {
+public interface PetService extends CrudRepository<Pet, Long> {
 
 }
